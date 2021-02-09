866b4a2c68196894ad4e175a7473f92e8f76c2bf
Kuei <ragnakuei@livemail.tw>
2021-02-09 10:11:03 +0800

    ok

---
+++ AspNetCoreMvcWithLightVue/Controllers/DatePickerController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreMvcWithLightVue.Controllers
+{
+    public class DatePickerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
