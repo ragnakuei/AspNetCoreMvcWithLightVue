adecb7209fd069584b43283e0682ac3c1259f4ba
Kuei <ragnakuei@livemail.tw>
2021-10-04 15:49:19 +0800

    新增引用 NLog

--- AspNetCoreMvcWithLightVue/Program.cs
+++ AspNetCoreMvcWithLightVue/Program.cs
@@ -4,8 +4,10 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using NLog.Web;
 
 namespace AspNetCoreMvcWithLightVue
 {
@@ -13,11 +15,16 @@ namespace AspNetCoreMvcWithLightVue
     {
         public static void Main(string[] args)
         {
+            var processPath    = AppDomain.CurrentDomain.BaseDirectory;
+            var nlogConfigPath = Path.Combine(processPath, "nlog.config");
+            var log            = NLogBuilder.ConfigureNLog(nlogConfigPath).GetCurrentClassLogger();
+
             CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseNLog()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
